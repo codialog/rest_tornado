b714aef2df61e3ef4253474dcb23fc5b8375bded
v.druzhinin <dialog.txt@gmail.com>
2020-02-08 13:07:33 +0700

    Added Post method

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
+import json
 from tornado.web import Application, RequestHandler
 from tornado.ioloop import IOLoop
 
+items = []
 
-class HelloHandler(RequestHandler):
+
+class TodoItems(RequestHandler):
     def get(self):
-        self.write({'massage': 'hello Tornado'})
+        write_message = []
+        for item in items:
+            write_message.append(json.loads(item))
+        self.write({'items': write_message})
+
+
+class TodoItem(RequestHandler):
+    def post(self):
+        items.append(self.request.body)
+        self.write({'message': 'new item added'})
 
 
 def make_app():
-    urls = [('/', HelloHandler)]
-    return Application(urls)
+    urls = [
+        ("/", TodoItems),
+        ("/api/item/", TodoItem)
+    ]
+    return Application(urls, debug=True)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = make_app()
     app.listen(3000)
     IOLoop.instance().start()
